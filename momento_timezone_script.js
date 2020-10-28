5543e5bb55e791774891f2e96c1e61cd50482674
Sandeep Yadav <55825136+yadavsndp@users.noreply.github.com>
2020-10-28 16:35:10 +0530

    Add files via upload

---
+++ momento_timezone_script.js
@@ -0,0 +1,23 @@
+<script> 
+  $(document).ready(function(){
+    moment.tz.add('Australia/Adelaide|ACST ACDT|-9u -au|0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101|-293lt xcX 10jd0 yL0 1cN0 1cL0 1fB0 19X0 17c10 LA0 1C00 Oo0 1zc0 Oo0 1zc0 Oo0 1zc0 Rc0 1zc0 Oo0 1zc0 Oo0 1zc0 Oo0 1zc0 Oo0 1zc0 Oo0 1zc0 Rc0 1zc0 Oo0 1zc0 Oo0 1zc0 Oo0 1zc0 U00 1qM0 WM0 1tA0 WM0 1tA0 U00 1tA0 U00 1tA0 Oo0 1zc0 WM0 1qM0 Rc0 1zc0 U00 1tA0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 11A0 1o00 WM0 1qM0 14o0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0|11e5"');
+	var EndDate = moment.tz("{{ section.settings.day_date_time }}", "Australia/Adelaide");
+                    
+      var second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+  	  
+	  var countDown = EndDate.toDate(),
+      x = setInterval(function() {
+      var now = new Date().getTime(),
+          distance = countDown - now;
+      document.querySelector('.days').innerText = Math.floor(distance / (day)),
+      document.querySelector('.hrs').innerText = Math.floor((distance % (day)) / (hour)),
+      document.querySelector('.min').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.querySelector('.sec').innerText = Math.floor((distance % (minute)) / second);
+    }, second)
+                           
+    });                         
+    
+</script>
